d818431632e6864ef717f1f4e5d7d07422fc3575
ewerthonrorigo <ewerthon.roriggo@gmail.com>
2019-11-12 11:15:31 -0300

    ATIVIDADE CALCULADORA PARA DIA 14 PROFESSOR RUBENS

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		int valor;
+		
+		Scanner teclado = new Scanner (System.in);
+		System.out.println("Digite valor da Tabuada: ");
+		valor = teclado.nextInt();
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(valor + " X " + i + " = " + (valor*i) );
+			
+		}
+
+	}
+
+}
